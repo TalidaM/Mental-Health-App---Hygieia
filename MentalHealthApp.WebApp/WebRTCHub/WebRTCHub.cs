a8991064eb4d959ae15f4ac0bcf975f6cfbfbbd6
Talida <108531743+TalidaM@users.noreply.github.com>
2022-10-03 13:53:27 +0300

    Add files via upload

---
+++ MentalHealthApp.WebApp/WebRTCHub/WebRTCHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MentalHealthApp.WebApp.WebRTCHub
+{
+    public class WebRTCHub : Hub
+    {
+        public async Task Join(int uuid)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, uuid.ToString());
+            await Clients.Group(uuid.ToString()).SendAsync("joinedRoom", uuid);
+        }
+
+        public async Task LeaveRoom(int roomId)
+        {
+            await Clients.Group(roomId.ToString()).SendAsync("leftRoom");
+        }
+
+        public async Task SendMessage(int roomId, object message)
+        {
+            await Clients.OthersInGroup(roomId.ToString()).SendAsync("message", message);
+        }
+    }
+}
